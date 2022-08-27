e40de03f4480e16fa383ab98916e02b87dca10a6
GiCharkviani <gcharkvi@broadinstitute.org>
2022-08-27 21:38:04 +0400

    today

--- OOP - Inheritance/src/Dog.java
+++ OOP - Inheritance/src/Dog.java
@@ -24,4 +24,23 @@ public class Dog extends Animal {
         chew();
         super.eat();
     }
+
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(12);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(24); // if move is not here, it will execute from parent, otherwise from here
+    }
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called");
+    }
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+    }
 }
