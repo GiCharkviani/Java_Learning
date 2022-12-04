5b8ef2f041cb427e009efe9179830ac6fd2744c5
GiCharkviani <gcharkvi@broadinstitute.org>
2022-12-04 15:27:34 +0400

    Setup finished for the Annotaitons

--- Java Framework (Hibernate & Spring)/SpringLearningAnnotations/src/com/gio/spring/AnnotationDemoApp.java
+++ Java Framework (Hibernate & Spring)/SpringLearningAnnotations/src/com/gio/spring/AnnotationDemoApp.java
@@ -1,9 +1,22 @@
 package com.gio.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
+		
+		// read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 	
+		// get the bean from spring container
+		Coach coach = context.getBean("tennisCoach", Coach.class);
+		
+		// call a method on the bean
+		System.out.print(coach.getDailyWorkout());
+		
+		// close the context
+		context.close();
 
 	}
 
