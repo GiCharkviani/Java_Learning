7db5b2f5a8169d1cce1f560a0052e693a1fcb580
GiCharkviani <gcharkvi@broadinstitute.org>
2022-12-19 20:54:45 +0400

    ss

--- Java Framework (Hibernate & Spring)/springMVCDemo/src/com/gio/Customer.java
+++ Java Framework (Hibernate & Spring)/springMVCDemo/src/com/gio/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.gio.validation.CourseCode;
+
 public class Customer {
 	
 	private String firstName;
@@ -23,29 +25,47 @@ public class Customer {
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
 	
+	@CourseCode(value="TOPS", message="must start with TOPS")
+	private String courseCode;
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public Integer getFreePasses() {
 		return freePasses;
 	}
+	
 	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
+	
 	public String getPostalCode() {
 		return postalCode;
 	}
+	
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}
 	
 	
