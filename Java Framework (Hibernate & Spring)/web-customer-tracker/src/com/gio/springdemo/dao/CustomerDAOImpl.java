0153e086dfc4914da6272531cbc30dd450f2806f
GiCharkviani <gcharkvi@broadinstitute.org>
2023-01-25 14:48:50 +0400

    finished project

--- Java Framework (Hibernate & Spring)/web-customer-tracker/src/com/gio/springdemo/dao/CustomerDAOImpl.java
+++ Java Framework (Hibernate & Spring)/web-customer-tracker/src/com/gio/springdemo/dao/CustomerDAOImpl.java
@@ -23,15 +23,42 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// get the current hibernate session
 		Session session = sessionFactory.getCurrentSession();
 		
-		// create a query
-		Query<Customer> theQuery = session.createQuery("from Customer", Customer.class);
+		// create a query .. sort by last name
+		Query<Customer> theQuery = session.createQuery("from Customer order by lastName", Customer.class);
 		
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
 		
 		// return the results
-		
 		return customers;
+	}
+	
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		// get current hibernate session
+		Session session = sessionFactory.getCurrentSession();
+		
+		// save or update the customer .. finally
+		session.saveOrUpdate(theCustomer);
+	}
+	
+	@Override
+	public Customer getCustomer(int theId) {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Customer customer = session.get(Customer.class, theId);
+		
+		return customer;
+	}
+	
+	@Override
+	public void deleteCustomer(int theId) {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query theQuery = session.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();
 	}
 
 }
--- Java Framework (Hibernate & Spring)/web-customer-tracker/src/com/gio/springdemo/service/CustomerService.java
+++ Java Framework (Hibernate & Spring)/web-customer-tracker/src/com/gio/springdemo/service/CustomerService.java
@@ -7,5 +7,11 @@ import com.gio.springdemo.entity.Customer;
 public interface CustomerService {
 	
 	public List<Customer> getCustomers();
+	
+	public void saveCustomer(Customer theCustomer);
+	
+	public Customer getCustomer(int theId);
+	
+	public void deleteCustomer(int theId);
 
 }
