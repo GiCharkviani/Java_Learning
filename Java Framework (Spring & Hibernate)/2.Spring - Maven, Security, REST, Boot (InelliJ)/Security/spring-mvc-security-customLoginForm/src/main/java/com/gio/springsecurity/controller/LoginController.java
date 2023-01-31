cae32bedeb5c6e5949d08d62acf105d18c846e89
GiCharkviani <gcharkvi@broadinstitute.org>
2023-01-31 16:54:21 +0400

    security on...

---
+++ Java Framework (Spring & Hibernate)/2.Spring - Maven, Security, REST, Boot (InelliJ)/Security/spring-mvc-security-customLoginForm/src/main/java/com/gio/springsecurity/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.gio.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+
+        return "home";
+    }
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+
+        return "plain-login";
+    }
+
+
+}
---
+++ Java Framework (Spring & Hibernate)/2.Spring - Maven, Security, REST, Boot (InelliJ)/Security/spring-mvc-security-customLoginForm/target/spring-mvc-security-customLoginForm/WEB-INF/view/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: giorgi.charkviani
+  Date: 30.01.23
+  Time: 20:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome user</h1>
+</body>
+</html>
