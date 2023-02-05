bd7c9bd5966298c0c55dbc68b4fc1d47ed984893
GiCharkviani <gcharkvi@broadinstitute.org>
2023-02-05 16:33:20 +0400

    JSON finshed

---
+++ Java Framework (Spring & Hibernate)/2.Spring - Maven, Security, REST, Boot (InelliJ)/REST/JSON/Jackson-databind-json/src/main/java/com/gio/jackson/json/Address.java
@@ -0,0 +1,62 @@
+package com.gio.jackson.json;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zip;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Address() {}
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
