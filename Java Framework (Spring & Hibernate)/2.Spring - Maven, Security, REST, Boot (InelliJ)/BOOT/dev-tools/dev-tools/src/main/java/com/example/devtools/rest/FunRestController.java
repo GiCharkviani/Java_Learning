117af14a35c614fbcc4dd7fdf1f7c3fd164115c5
GiCharkviani <gcharkvi@broadinstitute.org>
2023-02-08 10:54:04 +0400

    Boot learning done

--- Java Framework (Spring & Hibernate)/2.Spring - Maven, Security, REST, Boot (InelliJ)/BOOT/dev-tools/dev-tools/src/main/java/com/example/devtools/rest/FunRestController.java
+++ Java Framework (Spring & Hibernate)/2.Spring - Maven, Security, REST, Boot (InelliJ)/BOOT/dev-tools/dev-tools/src/main/java/com/example/devtools/rest/FunRestController.java
@@ -1,12 +1,21 @@
 package com.example.devtools.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -21,5 +30,11 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getFortune() {
         return "You are lucky!";
+    }
+
+    // expose new endpoint for "team info"
+    @GetMapping("/teaminfo/{yourName}")
+    public String getTeamInfo(@PathVariable String yourName) {
+        return "Coach: " + coachName + ", Team name: " + teamName + " >> Your name is: " + yourName;
     }
 }
