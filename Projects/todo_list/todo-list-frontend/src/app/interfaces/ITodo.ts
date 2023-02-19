e32e0d5812d6e41008c26eec79469bf5c7f89cbb
GiCharkviani <gcharkvi@broadinstitute.org>
2023-02-19 20:01:31 +0400

    project wokring process

--- Projects/todo_list/todo-list-frontend/src/app/interfaces/ITodo.ts
+++ Projects/todo_list/todo-list-frontend/src/app/interfaces/ITodo.ts
@@ -1,4 +1,5 @@
 export interface ITodo {
+  id: number;
   whatTodo: string;
-  whenTodo: string;
+  whenTodo: string | Date;
 }
--- Projects/todo_list/todo-list-frontend/src/app/services/htt.service.ts
+++ Projects/todo_list/todo-list-frontend/src/app/services/htt.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
 import {ITodo} from "../interfaces/ITodo";
 import {environment} from "../../environments/environment.prod";
 
@@ -10,6 +10,27 @@ export class HttService {
   }
 
   public getTodos(): Observable<ITodo[]> {
-    return this.httpClient.get<ITodo[]>(environment.restUrl);
+    return this.httpClient.get<ITodo[]>(environment.restUrl)
+      .pipe(tap((data) => console.log(data, 'GET ALL')));
+  }
+
+  public getTodo(id: number): Observable<ITodo> {
+    return this.httpClient.get<ITodo>(environment.restUrl + `/${id}`)
+      .pipe(tap((data) => console.log(data, 'GET ONE')));
+  }
+
+  public addTodo(todo: Partial<ITodo>): Observable<ITodo> {
+    return this.httpClient.post<ITodo>(environment.restUrl, todo)
+      .pipe(tap((data) => console.log(data, 'ADD')));
+  }
+
+  public updateTodo(todo: ITodo): Observable<ITodo> {
+    return this.httpClient.put<ITodo>(environment.restUrl, todo)
+      .pipe(tap((data) => console.log(data, 'UPDATE')));
+  }
+
+  public deleteTodo(id: number): Observable<ITodo> {
+    return this.httpClient.delete<ITodo>(environment.restUrl + `/${id}`)
+      .pipe(tap((data) => console.log(data, 'DELETE')));
   }
 }
