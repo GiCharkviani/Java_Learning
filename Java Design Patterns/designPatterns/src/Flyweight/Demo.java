00f502ff2784cc32dbc6e3250890d36476548a49
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-23 18:42:49 +0100

    dsada

---
+++ Java Design Patterns/designPatterns/src/Flyweight/Demo.java
@@ -0,0 +1,9 @@
+package Flyweight;
+/* Flyweight pattern */
+// A space optimization technique that lets us use less memory by storing
+// externally the data associated with similar objects
+
+
+public class Demo {
+
+}
