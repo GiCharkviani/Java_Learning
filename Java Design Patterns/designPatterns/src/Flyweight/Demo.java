ee2e3f801f5398d0bf41190b9674416252b04271
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-01-23 18:50:56 +0100

    dsada

--- Java Design Patterns/designPatterns/src/Flyweight/Demo.java
+++ Java Design Patterns/designPatterns/src/Flyweight/Demo.java
@@ -4,6 +4,45 @@ package Flyweight;
 // externally the data associated with similar objects
 
 
-public class Demo {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
+class User {
+    private String fullName;
+
+    public User(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
+class User2 {
+    static List<String> strings = new ArrayList<>();
+    private int[] names;
+
+    // Flyweight constructor
+    public User2(String fullName) {
+        Function<String, Integer> getOrAdd = (String s) -> {
+            int idx = strings.indexOf(s);
+            if (idx != -1) return idx;
+            else {
+                strings.add(s);
+                return strings.size() - 1;
+            }
+        };
+
+        names = Arrays.stream(fullName.split(" "))
+                .mapToInt(getOrAdd::apply)
+                .toArray();
+    }
+}
+
+
+public class Demo {
+    public static void main(String[] args) {
+        User johnSmith = new User("John Smith");
+        User janeSmith = new User("Jane Smith");
+
+    }
 }
---
+++ Java Design Patterns/designPatterns/src/Flyweight/TextFormatting.java
@@ -0,0 +1,8 @@
+package Flyweight;
+/* Flyweight pattern */
+// A space optimization technique that lets us use less memory by storing
+// externally the data associated with similar objects
+
+
+public class TextFormatting {
+}
