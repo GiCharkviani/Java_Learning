59724582d22418224c5d32e756ba168ad785a10e
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-11 19:03:02 +0100

    gfdgdgd

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section2/ifKeywordANdCodeBlocks/IfKeywordANdCodeBlocks.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section2/ifKeywordANdCodeBlocks/IfKeywordANdCodeBlocks.java
@@ -2,6 +2,24 @@ package Section2.ifKeywordANdCodeBlocks;
 
 public class IfKeywordANdCodeBlocks {
     public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 0;
+        int levelCompleted = 3;
+        int bonus = 100;
 
+        calculateScore(gameOver, score, levelCompleted, bonus);
     }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is " + finalScore);
+        }
+    }
+
+    
+
+
+
 }
