f70e01a5ac425c387d8b0214da3c39eaec33a3ad
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-11 17:35:12 +0100

    fdsfs

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section1/PrimitiveTypes/PrimitiveTypes.java
@@ -0,0 +1,36 @@
+package Section1.PrimitiveTypes;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        // Characters - 16 bits (2 Bytes)
+        char myChar = 'a';
+        char yourChar = '\u005A';
+
+        // Boolean - 1 bit
+        boolean amI = true;
+
+        // Number types
+        // Byte 8 bits (1 byte)
+        byte catAge = 127;
+
+        // Integer - 32 bits (4 bytes)
+        int myAge = 21;
+        int yourAge = 9 / 2;
+
+        // Long - 64 bit (8 bytes)
+        long years = 23123131;
+
+        // Float - 32 bits (4 bytes)
+        float price = 23.3f;
+        float breadPrice = 2.2f - 1.1f;
+
+        // Double - 64 bit (8 bytes)
+        double milkPrice = 2.2 - 1.1;
+
+
+        System.out.println(appleKgs);
+    }
+
+
+}
