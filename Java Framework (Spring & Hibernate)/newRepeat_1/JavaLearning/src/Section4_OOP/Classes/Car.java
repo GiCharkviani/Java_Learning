7f6023be08af00d24061c2045fe7686fe0564551
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-12 19:17:10 +0100

    gfdgd

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Classes/Car.java
@@ -0,0 +1,12 @@
+package Section4_OOP.Classes;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    
+
+}
