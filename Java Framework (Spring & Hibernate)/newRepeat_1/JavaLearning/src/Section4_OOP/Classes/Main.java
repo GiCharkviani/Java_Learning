b1e62385dfa78f875c5051ebcaa6e497d21a02ce
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-12 19:26:02 +0100

    gfdgd

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Classes/Main.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Classes/Main.java
@@ -2,6 +2,8 @@ package Section4_OOP.Classes;
 
 public class Main {
     public static void main(String[] args) {
+        Car BMW = new Car("M5", "N58");
 
+        System.out.println(BMW);
     }
 }
