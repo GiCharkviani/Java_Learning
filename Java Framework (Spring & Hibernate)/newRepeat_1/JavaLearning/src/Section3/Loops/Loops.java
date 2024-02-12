a02dd5e623c9f38cf6f88cdc655be076d83b7e7f
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-12 18:51:15 +0100

    gfdgd

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section3/Loops/Loops.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section3/Loops/Loops.java
@@ -5,6 +5,17 @@ public class Loops {
 
         for (int i = 0; i < 100; i++) {
             System.out.println("I am " + i + " number");
+            if(i == 10)
+                break;
+        }
+
+        System.out.println("**********");
+
+        int myNumber = 0;
+
+        while (myNumber < 10) {
+            System.out.println("Number is " + myNumber);
+            myNumber++;
         }
 
     }
