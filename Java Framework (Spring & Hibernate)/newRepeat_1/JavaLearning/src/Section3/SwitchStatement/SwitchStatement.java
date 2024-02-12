9c435d1b5878e14a1e6e1545d5ee63ce55b74dc7
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-12 18:44:45 +0100

    gfdgd

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section3/SwitchStatement/SwitchStatement.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section3/SwitchStatement/SwitchStatement.java
@@ -1,4 +1,22 @@
 package Section3.SwitchStatement;
 
 public class SwitchStatement {
+    public static void main(String[] args) {
+        int switchValue = 4;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 5:
+                System.out.println("Value is 5");
+                break;
+            case 4:
+                System.out.println("Value is 4");
+                break;
+            default:
+                System.out.println("Invalid Value");
+                break;
+        }
+    }
 }
