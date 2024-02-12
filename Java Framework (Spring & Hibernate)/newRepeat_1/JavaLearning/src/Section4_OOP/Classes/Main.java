e7b71c0fff6edaf7226c0c52b7a9a5d8ad78e014
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-12 20:13:30 +0100

    fdsfds

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Classes/Main.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Classes/Main.java
@@ -2,8 +2,9 @@ package Section4_OOP.Classes;
 
 public class Main {
     public static void main(String[] args) {
-        Car BMW = new Car("M5", "N58");
+        Car.logCar();
 
+        Car BMW = new Car("M5", "N58");
         BMW.setColor("red");
         System.out.println(BMW.getColor());
     }
