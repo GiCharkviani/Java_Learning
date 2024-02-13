19afe45d74804b13bd940a7d6ddcd899bd16b60d
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-13 20:15:57 +0100

    gfdgdg

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Inheritance/Main.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Inheritance/Main.java
@@ -2,6 +2,13 @@ package Section4_OOP.Inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Boing777 ourBoing = new Boing777("FLying Dragon", 666, "V200", 4, 6);
+        ourBoing.setModel("777");
+        ourBoing.setTurbines(200);
+
+        ourBoing.fly();
+        ourBoing.steering();
+
     }
 }
