f10d80a84237e7b68d5b51d61238dd925440775a
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-13 19:47:04 +0100

    gfdgd

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Inheritance/Airplane.java
@@ -0,0 +1,11 @@
+package Section4_OOP.Inheritance;
+
+public class Airplane extends Vehicle {
+    private int wings;
+    private int turbines;
+
+    public void fly() {
+        System.out.println(this.name + " is flying");
+    }
+
+}
