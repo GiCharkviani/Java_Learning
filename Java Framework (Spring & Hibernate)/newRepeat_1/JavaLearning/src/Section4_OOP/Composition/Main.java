5d3dbd97d79cd018f9890abbee0f716b7b51c42a
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-13 21:25:48 +0100

    dsadas

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Composition/Main.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Composition/Main.java
@@ -5,8 +5,12 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Motherboard myMotherboard = new Motherboard("xlxxs", "ASUS", 21, 32, 2, 4, 4, "Intel Bios");
-        DigitalProduct myDigitalProduct = new DigitalProduct("Rgac", "vigac", 21, 32, 3, "version 1", new Date());
+        DigitalProduct myDigitalProduct = new DigitalProduct("Rgac", "vigac", "version 1", new Date());
 
+        myMotherboard.logProductModel();
 
+        // Using composition
+        Demensions myMBDemenesions = myMotherboard.getDemensions();
+        System.out.println(myMBDemenesions.getDepth());
     }
 }
