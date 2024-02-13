98a36ece7b37b4e47922bb679ca5c6bdd6768992
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-13 21:14:38 +0100

    fsdffs

--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Composition/Demensions.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Composition/Demensions.java
@@ -1,5 +1,25 @@
 package Section4_OOP.Composition;
 
 public class Demensions {
+    private int width;
+    private int height;
+    private int depth;
 
+    public Demensions(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
 }
--- Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Composition/Product.java
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Composition/Product.java
@@ -3,16 +3,11 @@ package Section4_OOP.Composition;
 public class Product {
     private String model;
     private String manufacturer;
-    private int width;
-    private int height;
-    private int depth;
 
-    public Product(String model, String manufacturer, int width, int height, int depth) {
+
+    public Product(String model, String manufacturer) {
         this.model = model;
         this.manufacturer = manufacturer;
-        this.width = width;
-        this.height = height;
-        this.depth = depth;
     }
 
     public void logProductModel() {
@@ -25,17 +20,5 @@ public class Product {
 
     public String getManufacturer() {
         return manufacturer;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public int getDepth() {
-        return depth;
     }
 }
