6f74913df12f9ce75d47217633be14af5a197bf8
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-13 20:08:58 +0100

    fdsfsf

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Inheritance/Boing777.java
@@ -0,0 +1,17 @@
+package Section4_OOP.Inheritance;
+
+public class Boing777 extends Airplane {
+    private String model;
+
+    public Boing777(String name, int size, String engine, int wheels, int wings) {
+        super(name, size, engine, wheels, wings);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
