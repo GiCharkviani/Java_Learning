92534bfd9398710f8f2add6b228c12cdcb0ae3ce
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-13 19:38:15 +0100

    gfdgd

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Inheritance/Vehicle.java
@@ -0,0 +1,35 @@
+package Section4_OOP.Inheritance;
+
+public class Vehicle {
+    private String name;
+    private String engine;
+
+    private int wheels;
+    private int size;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public Vehicle(String name, int size, String engine) {
+        this(name);
+        this.size = size;
+        this.engine = engine;
+    }
+
+    public Vehicle(String name, int size, String engine, int wheels) {
+        this(name, size, engine);
+        this.wheels = wheels;
+    }
+
+    public void move() {
+        System.out.println(this.name + " vehicle is moving");
+    }
+
+    public void steering() {
+        System.out.println(this.name + " vehicle is steering");
+    }
+
+
+
+}
