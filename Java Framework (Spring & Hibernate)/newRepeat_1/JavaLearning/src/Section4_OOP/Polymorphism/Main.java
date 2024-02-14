fd61c79f2afab23e73a3e2913172fde076e12956
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-14 19:00:52 +0100

    fsdfsfd

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Polymorphism/Main.java
@@ -0,0 +1,24 @@
+package Section4_OOP.Polymorphism;
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public void plot() {
+        System.out.println("No Plot Here");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
