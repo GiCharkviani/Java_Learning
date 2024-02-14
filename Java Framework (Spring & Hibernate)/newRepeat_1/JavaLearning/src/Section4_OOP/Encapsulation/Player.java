50991714de34170165cd25359a4bbf24b2bacb68
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-14 18:41:18 +0100

    fdsfsf

---
+++ Java Framework (Spring & Hibernate)/newRepeat_1/JavaLearning/src/Section4_OOP/Encapsulation/Player.java
@@ -0,0 +1,19 @@
+package Section4_OOP.Encapsulation;
+
+public class Player {
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health -= damage;
+        if(this.health <= 0) {
+            System.out.println("Player is dead");
+        }
+    }
+
+
+    public int healthRemaining() {
+        return this.health;
+    }
+}
