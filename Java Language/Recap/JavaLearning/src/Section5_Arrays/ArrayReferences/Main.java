b6028914da8836e99c449248397d4f745e7df80f
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-15 20:53:48 +0100

    gfdgd

--- Java Language/Recap/JavaLearning/src/Section5_Arrays/ArrayReferences/Main.java
+++ Java Language/Recap/JavaLearning/src/Section5_Arrays/ArrayReferences/Main.java
@@ -2,6 +2,19 @@ package Section5_Arrays.ArrayReferences;
 
 public class Main {
     public static void main(String[] args) {
+        int[] myNumbers = new int[10];
+        String myName = "Giorgi";
 
+        int[] yourNumber = myNumbers;
+        yourNumber[0] = 10;
+
+        System.out.println(myNumbers[0]);
+
+    }
+
+    public static void logIntegersArray(int[] integers) {
+        for (int i = 0; i < integers.length; i++) {
+            System.out.println(integers[i]);
+        }
     }
 }
