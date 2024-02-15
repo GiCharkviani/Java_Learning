8eadf963dd46639d1b106bcb3506bbaaebb2faaf
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-15 20:15:14 +0100

    hgfhfhgf

--- Java Language/Recap/JavaLearning/src/Section5_Arrays/UsingArrays/Main.java
+++ Java Language/Recap/JavaLearning/src/Section5_Arrays/UsingArrays/Main.java
@@ -1,7 +1,24 @@
 package Section5_Arrays.UsingArrays;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+        int[] myNumbers = new int[10];
+        int[] ages = new int[]{32,43,12,54,31,21};
+        int[] years = {21, 32, 43, 21, 43}; // Anonymous array
+
+        System.out.println(myNumbers[0]);
+        System.out.println(ages[0]);
+
+        myNumbers[0] = 55;
+        ages[0] = 0;
+        years[0] = 66;
+
+        System.out.println("*******");
+
+        System.out.println(myNumbers[0]);
+        System.out.println(ages[0]);
+        System.out.println(years[0]);
+
     }
 }
