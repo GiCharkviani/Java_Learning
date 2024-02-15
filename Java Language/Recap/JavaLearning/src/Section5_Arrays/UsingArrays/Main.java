6f88c0f7138ec122f0f05ae827003e29dc7f49b1
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-15 20:24:39 +0100

    gfdgd

--- Java Language/Recap/JavaLearning/src/Section5_Arrays/UsingArrays/Main.java
+++ Java Language/Recap/JavaLearning/src/Section5_Arrays/UsingArrays/Main.java
@@ -9,23 +9,25 @@ public class Main {
         int[] ages = new int[]{32,43,12,54,31,21};
         int[] years = {21, 32, 43, 21, 43}; // Anonymous array
 
-        System.out.println(myNumbers[0]);
-        System.out.println(ages[0]);
+//        System.out.println(myNumbers[0]);
+//        System.out.println(ages[0]);
 
         myNumbers[0] = 55;
         ages[0] = 0;
         years[0] = 66;
 
+//        System.out.println("*******");
+//
+//        System.out.println(myNumbers[0]);
+//        System.out.println(ages[0]);
+//        System.out.println(years[0]);
+//
+//        System.out.println("*******");
+
+        logArray(ages);
+        Arrays.sort(ages);
         System.out.println("*******");
-
-        System.out.println(myNumbers[0]);
-        System.out.println(ages[0]);
-        System.out.println(years[0]);
-
-        System.out.println("*******");
-
-        Arrays.sort(myNumbers);
-
+        logArray(ages);
     }
 
     public static void logArray(int[] integers) {
