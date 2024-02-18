d8ba80f5cf51a08ccaf59ec8291130c49c8385b6
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-18 01:58:02 +0100

    dsada

---
+++ Java Language/Recap/JavaLearning/src/SwapArray/Main.java
@@ -0,0 +1,30 @@
+package SwapArray;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 4, 1, 3, 5, 12, 32, 11};
+
+//       log(numbers);
+       reverse(numbers);
+        System.out.println("*****");
+       log(numbers);
+    }
+
+    public static void reverse(int[] array) {
+        int base = 0;
+        for (int i = array.length - 1; ; i--) {
+            if(i <= base)
+                break;
+            int temp = array[base];
+            array[base] = array[i];
+            array[i] = temp;
+            base++;
+        }
+    }
+
+    public static void log(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+}
