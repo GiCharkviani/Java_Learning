cd2ac08c385310e1c283337d72c9e330f37a6879
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-19 21:42:54 +0100

    fdsfs

---
+++ Java Language/Recap/JavaLearning/src/Section6_ArrayList_LinkedList_Iterator_Autoboxing/AutoboxingInboxing/Main.java
@@ -0,0 +1,15 @@
+package Section6_ArrayList_LinkedList_Iterator_Autoboxing.AutoboxingInboxing;
+
+public class Main {
+    public static void main(String[] args) {
+//        LinkedList<int> numbers = new LinkedList<int>(); // Will throw error
+        Integer myAge = 21; // Autoboxing
+        Double amount = Double.valueOf(33.2); // boxing
+        Integer number = Integer.valueOf(22);
+
+        int myNewAge = myAge.intValue(); // unboxing
+
+
+        System.out.println(myAge);
+    }
+}
