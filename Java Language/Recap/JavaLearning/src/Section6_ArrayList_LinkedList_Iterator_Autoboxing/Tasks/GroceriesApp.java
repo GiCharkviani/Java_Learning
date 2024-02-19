04c30369bf3919e2385773f0ad497ce433afe245
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-19 20:01:15 +0100

    fdsfsdf

--- Java Language/Recap/JavaLearning/src/Section6_ArrayList_LinkedList_Iterator_Autoboxing/Tasks/GroceriesApp.java
+++ Java Language/Recap/JavaLearning/src/Section6_ArrayList_LinkedList_Iterator_Autoboxing/Tasks/GroceriesApp.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 public class GroceriesApp {
     public static void main(String[] args) {
         ArrayList<String> groceries = new ArrayList<>();
-        
+
+
+    }
+
+    public static void addItem(String item, ArrayList<String> list) {
+        if(!list.contains(item)) {
+            list.add(item);
+        } else {
+            System.out.println("The item " + item + " is already in the list");
+        }
     }
 }
