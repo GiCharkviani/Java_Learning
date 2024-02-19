f72b7177ecf86f595926d87e74fded36ab68bdf5
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-19 16:49:48 +0100

    fsdfds

--- Java Language/Recap/JavaLearning/src/SwapArray/Main.java
+++ Java Language/Recap/JavaLearning/src/SwapArray/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 4, 1, 3, 5, 12, 32, 11};
 
-//       log(numbers);
-       reverse(numbers);
+       log(numbers);
+//       reverse(numbers);
+        sortDesc(numbers);
         System.out.println("*****");
        log(numbers);
     }
@@ -18,6 +19,18 @@ public class Main {
             int temp = array[base];
             array[base] = array[i];
             array[i] = temp;
+        }
+    }
+
+    public static void sortDesc(int[] array) {
+        for (int outer  = 0; outer < array.length; outer++) {
+            for(int inner = 0; inner < array.length; inner++) {
+                if(array[outer] > array[inner]) {
+                    int temp = array[outer];
+                    array[outer] = array[inner];
+                    array[inner] = temp;
+                }
+            }
         }
     }
 
