f0dfec6cae68cd916f12068882aaa79dc2e62c72
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-20 21:09:00 +0100

    dsada

--- Java Language/Recap/JavaLearning/src/Section7_Abstraction/Interfaces/FlightEnabled.java
+++ Java Language/Recap/JavaLearning/src/Section7_Abstraction/Interfaces/FlightEnabled.java
@@ -1,6 +1,9 @@
 package Section7_Abstraction.Interfaces;
 
 public interface FlightEnabled {
+    double MILES_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
+
     void fly();
     void takeOff();
     void land();
