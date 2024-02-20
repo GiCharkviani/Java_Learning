9d1f8eb15832c086c0feb74f3e72c143b000a6d8
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-20 19:36:03 +0100

    fsdfsf

--- Java Language/Recap/JavaLearning/src/Section7_Abstraction/AbstractClasses/Animal.java
+++ Java Language/Recap/JavaLearning/src/Section7_Abstraction/AbstractClasses/Animal.java
@@ -2,12 +2,16 @@ package Section7_Abstraction.AbstractClasses;
 
 public abstract class Animal {
     protected String name;
-    protected String size;
-    protected  double weight;
+    private String size;
+    private double weight;
 
     public Animal(String name, String size, double weight) {
         this.name = name;
         this.size = size;
         this.weight = weight;
     }
+
+    public abstract void move(String speed);
+    public abstract void makeNoise();
+
 }
--- Java Language/Recap/JavaLearning/src/Section7_Abstraction/AbstractClasses/Dog.java
+++ Java Language/Recap/JavaLearning/src/Section7_Abstraction/AbstractClasses/Dog.java
@@ -1,4 +1,19 @@
 package Section7_Abstraction.AbstractClasses;
 
 public class Dog extends Animal {
+
+    public Dog(String name, String size, double weight) {
+        super(name, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+        System.out.println("Animal " + this.name + " is moving by speed " + speed);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Animal " + this.name + " makes noise wah-wah");
+    }
+
 }
