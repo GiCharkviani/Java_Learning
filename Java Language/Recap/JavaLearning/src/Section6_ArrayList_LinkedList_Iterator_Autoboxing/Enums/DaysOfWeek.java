2d5b131491177bef277f6d52271252802d353817
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-20 18:47:07 +0100

    dsada

---
+++ Java Language/Recap/JavaLearning/src/Section6_ArrayList_LinkedList_Iterator_Autoboxing/Enums/DaysOfWeek.java
@@ -0,0 +1,11 @@
+package Section6_ArrayList_LinkedList_Iterator_Autoboxing.Enums;
+
+public enum DaysOfWeek {
+    SUN,
+    MON,
+    TUES,
+    WED,
+    THURS,
+    FRI,
+    SAT
+}
--- Java Language/Recap/JavaLearning/src/Section6_ArrayList_LinkedList_Iterator_Autoboxing/Enums/Main.java
+++ Java Language/Recap/JavaLearning/src/Section6_ArrayList_LinkedList_Iterator_Autoboxing/Enums/Main.java
@@ -4,4 +4,30 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static void weekDay(DaysOfWeek weekDay) {
+        switch (weekDay) {
+            case SUN:
+                System.out.println("Today is a Sunday");
+                break;
+            case MON:
+                System.out.println("Today is a Monday");
+                break;
+            case TUES:
+                System.out.println("Today is a Tuesday");
+                break;
+            case WED:
+                System.out.println("Today is a Wednesday");
+                break;
+            case THURS:
+                System.out.println("Today is a Thursday");
+                break;
+            case FRI:
+                System.out.println("Today is a Friday");
+                break;
+            case SAT:
+                System.out.println("Today is a Saturday");
+                break;
+        }
+    }
 }
