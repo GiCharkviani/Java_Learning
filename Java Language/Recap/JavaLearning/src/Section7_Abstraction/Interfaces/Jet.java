e195634a66162a7709d28e6a8328be1d4525a63b
GiCharkviani <77500504+GiCharkviani@users.noreply.github.com>
2024-02-20 21:15:47 +0100

    gdfgd

---
+++ Java Language/Recap/JavaLearning/src/Section7_Abstraction/Interfaces/Jet.java
@@ -0,0 +1,23 @@
+package Section7_Abstraction.Interfaces;
+
+public class Jet implements FlightEnabled, Trackable {
+    @Override
+    public void fly() {
+        System.out.println("Jet is flying");
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println("Jet is taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println("Jet is landing");
+    }
+
+    @Override
+    public void track() {
+        System.out.println("Jet is tracking");
+    }
+}
